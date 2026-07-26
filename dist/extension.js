2c080ba4029ebbc9a2a608ad1c4092fa30058051
krishna bharadwaj ms <krishnabharadwajms@gmail.com>
2026-07-26 18:04:06 +0530

    Initialize VS Code extension with build setup
    Set up BackseatCoders as a VS Code extension with TypeScript, ESLint, and esbuild configuration. Includes compiled extension code that displays reactions on file save and a hello world command. Added package.json with dev dependencies and scripts for compilation, testing, and linting.

---
+++ dist/extension.js
@@ -0,0 +1,65 @@
+"use strict";
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+  // If the importer is in node compatibility mode or this is not an ESM
+  // file that has been converted to a CommonJS file using a Babel-
+  // compatible transform (i.e. "__esModule" has not been set), then set
+  // "default" to the CommonJS "module.exports" for node compatibility.
+  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+  mod
+));
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+// src/extension.ts
+var extension_exports = {};
+__export(extension_exports, {
+  activate: () => activate,
+  deactivate: () => deactivate
+});
+module.exports = __toCommonJS(extension_exports);
+var vscode = __toESM(require("vscode"));
+function activate(context) {
+  console.log("New Version DAWG!");
+  const messages = [
+    "\u{1F480} Bro is cooking.",
+    "\u{1F525} LET HIM COOK!",
+    "\u{1F431} meow",
+    "\u{1F602} Stack Overflow speedrun initiated.",
+    "\u{1F913} Interesting approach..."
+  ];
+  const saveListener = vscode.workspace.onDidSaveTextDocument((document) => {
+    const randomIndex = Math.floor(Math.random() * messages.length);
+    vscode.window.showInformationMessage(messages[randomIndex]);
+    const code = document.getText();
+    console.log(code);
+  });
+  const disposable = vscode.commands.registerCommand("backseatcoders.helloWorld", () => {
+    vscode.window.showInformationMessage("its is working");
+  });
+  context.subscriptions.push(disposable, saveListener);
+}
+function deactivate() {
+}
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
+  activate,
+  deactivate
+});
+//# sourceMappingURL=extension.js.map
